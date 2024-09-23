1d14e1eff6187df7f4ac4bcb436411f10728f23c
rahulmedicharla <rmedicharla@gmail.com>
2024-09-23 16:38:51 -0400

    templatized volume mounts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -129,8 +129,8 @@ helmify(){
             -i -e "s#{{HOST}}#$hostname#" \
             "$CURRENT_DIR/$NAME/deploy/values.yaml"
         
-        # yq e ".volumeMounts += [{\"name\": \"traefik-config\", \"mountPath\": \"/config\"}]" $CURRENT_DIR/$NAME/deploy/values.yaml -i
-        # yq e ".volumes += [{\"name\": \"traefik-config\", \"configMap\": {\"name\": \"traefik-config\"}}]" $CURRENT_DIR/$NAME/deploy/values.yaml -i
+        yq e ".volumeMounts += [{\"name\": \"traefik-config\", \"mountPath\": \"/config\"}]" $CURRENT_DIR/$NAME/deploy/values.yaml -i
+        yq e ".volumes += [{\"name\": \"traefik-config\", \"configMap\": {\"name\": \"traefik-config\"}}]" $CURRENT_DIR/$NAME/deploy/values.yaml -i
     }
 
     helmify_api(){
